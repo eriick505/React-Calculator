a53e650146713264abeef1cac3ddc738472fadc7
eriick505 <44848532+eriick505@users.noreply.github.com>
2019-12-30 18:01:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './main/Calculator';
+import './styles/Global.css';
+
+function App() {
+  return (
+    <> 
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
